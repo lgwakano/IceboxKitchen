60f3f2aac0a987298a5ae3e1acfd3f83a9176e10
Luiz Wakano <lgwakano@gmail.com>
2026-07-16 16:48:12 -0600

    feat(mapping): add mappings for Register and Login requests

--- IceboxKitchen.Api/Common/Mapping/AuthenticationMappingConfig.cs
+++ IceboxKitchen.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -1,4 +1,6 @@
+using IceboxKitchen.Application.Authentication.Commands.Register;
 using IceboxKitchen.Application.Authentication.Common;
+using IceboxKitchen.Application.Authentication.Queries.Login;
 using IceboxKitchen.Contracts.Authentication;
 using Mapster;
 
@@ -8,6 +10,9 @@ public class AuthenticationMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+        config.NewConfig<LoginRequest, LoginQuery>();
+
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
             .Map(dest => dest, src => src.User);
     }
